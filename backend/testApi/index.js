5213132c33b9a2547698c203c362227242ceb732
Pratim Khan <90311707+pratim-khan@users.noreply.github.com>
2021-11-23 10:56:23 +0530

    new file

---
+++ backend/testApi/index.js
@@ -0,0 +1,50 @@
+
+const MongoClient = require('../mongo/mongo');
+
+module.exports = async function (context, req) {
+
+
+  const { db, connection } = await MongoClient()
+
+  const Bill = db.collection('bill')
+  const data = req.body
+  const total= await Bill.aggregate(
+      [
+          {$match:{isdelete:false}},
+        //   {total:{$sum: '$amount'}}
+        {$group:{_id:null,total:{$sum:"$amount"}}}
+      ]
+  ).toArray()
+  const test =await Bill.aggregate(
+      [
+          {$match: {$and:[{name: data},{isdelete:false}]}},
+          {$group:{_id:null,total:{$sum:'$amount'}}}
+      ]
+  ).toArray()
+
+    if(test.length>=1){
+        try{
+            let avg = parseInt((total[0].total)/6)
+            let res = test[0].total
+            let due = (res-avg)
+            context.res={
+                body: {
+                    due:due,
+                }
+            }
+        }catch(error){
+            body="cannot get data"
+}
+
+}else{
+    let  avg = (total[0].total)/6
+    context.res={
+        body:{
+            due:avg
+        }
+    }
+}
+    
+    
+    
+}
---
+++ frontend/src/app/mat-dialog/mat-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-dialog',
+  templateUrl: './mat-dialog.component.html',
+  styleUrls: ['./mat-dialog.component.css']
+})
+export class MatDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
